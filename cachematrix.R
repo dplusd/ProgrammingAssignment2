de3f8c4b63a890173bd95ad8c47d875c9e61a660
Didi <dplusd@gmail.com>
2015-11-19 15:36:26 +0200

    Modified R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Instead of adding a setter and getter to the inversed matrix, I've implemented one function that calculates the 
+## matrix inverse from withing the makeCachedMatrix function,
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    inverse <- NULL
+    set <- function(y = matrix()) {       
+        x  <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function () {
+        x
+    }
+    
+    getInverse <- function () inverse
+    
+    setInverse <- function(inv) {
+        inverse <<- inv
+        
+    }
+    
+    list(set = set, get = get, getInverse = getInverse, setInverse = setInverse)
 }
 
 
-## Write a short comment describing this function
+## This function only calls the inverse() function of makeCacheMatrix. Caching logic was implemented  within the makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+    if(is.null(x$getInverse())) {
+        x$setInverse(solve(x$get()))
+    }
+    x$getInverse()
 }
